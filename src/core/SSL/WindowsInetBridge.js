febae8ed992234870ef8d20780c58729587c2fda
BurakDev <burak@burak.fr>
2020-12-22 18:42:30 +0100

    Use InetOptionsCLI release build instead of debug

--- src/core/SSL/WindowsInetBridge.js
+++ src/core/SSL/WindowsInetBridge.js
@@ -1,9 +1,15 @@
 const execSync = require('child_process').execSync;
 const path = require('path');
+const fs = require('fs');
 
 class WindowsInetBridge {
 	constructor() {
-		this.binPath = path.join(__dirname, '../../InetOptionsCLI/InetOptionsCLI/bin/Debug/InetOptionsCLI.exe');
+		this.binPath = path.join(__dirname, '../../InetOptionsCLI/InetOptionsCLI/bin/Release/InetOptionsCLI.exe');
+
+		if (!fs.existsSync(this.binPath)) {
+			alert('Please compile Windows inet dotnet project, program will shutdown now.');
+			require('electron').remote.getCurrentWindow().close();
+		}
 	}
 
 	setProxy(proxyAddress) {
