e2376fcb80dfa9cd2872966d4ee25cd81e2b336d
BurakDev <burak@burak.fr>
2020-12-27 22:15:31 +0100

    Work on auto-updater

--- main.js
+++ main.js
@@ -1,68 +1,67 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const electron = require('electron');
 const path = require('path');
 
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 600,
-    height: 400,
-    resizable: false,
-    alwaysOnTop: false,
-    webPreferences:{
-      nodeIntegration: true,
-      nodeIntegrationInWorker: true
-    },
-    icon: path.join(__dirname, 'icon.png')
-  });
+	mainWindow = new BrowserWindow({
+		width: 600,
+		height: 400,
+		resizable: false,
+		alwaysOnTop: false,
+		show: false,
+		webPreferences:{
+			nodeIntegration: true,
+			nodeIntegrationInWorker: true
+		},
+		icon: path.join(__dirname, 'icon.png')
+	});
 
-  //mainWindow.setMenu(null);
+	//mainWindow.setMenu(null);
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/src/view/index.html`);
+	mainWindow.loadURL(`file://${__dirname}/src/view/index.html`);
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+		autoUpdater.checkForUpdatesAndNotify();
+	});
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  //if (process.platform !== 'darwin') {
-    app.quit()
-  //}
+app.on('ready', () => {
+	createWindow();
 });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('window-all-closed', () => {
+	app.quit();
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
+
+ipcMain.on('update_app', () => {
+	autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('update-available', () => {
+	console.log('update available');
+});
+
+autoUpdater.on('checking-for-update', () => {
+	console.log('checking for update');
+});
+
+autoUpdater.on('error', error => {
+	console.error('ERROR', error);
+});
